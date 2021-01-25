d3146ee48104bceb4417e671de9fa2de3ecfbe9d
William Johnson <wcjohns@sandia.gov>
2021-01-25 13:33:19 -0800

    Add a "simple dialog" class. This simple dialog is intended for things like prompting a user a yes/no answer. Only minimally implemented, and stylistically not well integrated into the rest of the app yet.

---
+++ InterSpec/SimpleDialog.h
@@ -0,0 +1,76 @@
+#ifndef SimpleDialog_h
+#define SimpleDialog_h
+/* InterSpec: an application to analyze spectral gamma radiation data.
+ 
+ Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC
+ (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+ Government retains certain rights in this software.
+ For questions contact William Johnson via email at wcjohns@sandia.gov, or
+ alternative emails of interspec@sandia.gov.
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "InterSpec_config.h"
+
+#include <Wt/WDialog>
+#include <Wt/WString>
+
+namespace Wt
+{
+  class WPushButton;
+  class WContainerWidget;
+}//namespace Wt
+
+/** A simple minimal dialog meant to ask the user a question modal question where user should respond before continuing.
+   Kinda similar to a iOS dialog asking a question.
+ 
+ Defaults to centered in the middle of the screen.
+ */
+class SimpleDialog : public Wt::WDialog
+{
+public:
+  enum SimpleDialogProperties
+  {
+    // Show modal background
+    // Allow clicking on background to dismiss
+    // Allow escape key to dismiss
+  };//enum SimpleDialogProperties
+
+
+  SimpleDialog();
+  SimpleDialog( const Wt::WString &title );
+  SimpleDialog( const Wt::WString &title, const Wt::WString &content );
+  ~SimpleDialog();
+  
+  /** Add a button to the footer.
+   
+   Buttons are added left-to-right, and clicking on them will cause the dialog to hide and become deleted, so you dont need to worry
+   about cleaning up the dialog.
+   
+   Hookup to the returned button to trigger actions after clicking.
+   */
+  Wt::WPushButton *addButton( const Wt::WString &txt );
+  
+protected:
+  virtual void render( Wt::WFlags<Wt::RenderFlag> flags );
+  
+  void init( const Wt::WString &title, const Wt::WString &content );
+  static void deleteSimpleDialog( SimpleDialog *d );
+};//class SimpleDialog
+
+
+#endif //SimpleDialog_h
+
---
+++ src/SimpleDialog.cpp
@@ -0,0 +1,167 @@
+/* InterSpec: an application to analyze spectral gamma radiation data.
+ 
+ Copyright 2018 National Technology & Engineering Solutions of Sandia, LLC
+ (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+ Government retains certain rights in this software.
+ For questions contact William Johnson via email at wcjohns@sandia.gov, or
+ alternative emails of interspec@sandia.gov.
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "InterSpec_config.h"
+
+#include <Wt/WDialog>
+#include <Wt/WServer>
+#include <Wt/WPushButton>
+#include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+
+#include <string>
+
+//#include "InterSpec/InterSpec.h"  //to get rendered width/height
+#include "InterSpec/SimpleDialog.h"
+
+
+using namespace std;
+using namespace Wt;
+
+SimpleDialog::SimpleDialog()
+: Wt::WDialog( nullptr )
+{
+  init( "", "" );
+}
+
+SimpleDialog::SimpleDialog( const Wt::WString &title )
+ : Wt::WDialog( nullptr )
+{
+  init( title, "" );
+}
+
+
+SimpleDialog::SimpleDialog( const Wt::WString &title, const Wt::WString &content )
+ : Wt::WDialog( nullptr )
+{
+  init( title, content );
+}
+
+
+void SimpleDialog::render( Wt::WFlags<Wt::RenderFlag> flags )
+{
+  if( flags & RenderFull )
+  {
+    // WDialog::setMaximumSize will silently not use dimensions if WLength::Percentage
+    //  Note that page dimensions wont be available during initial rendering of the webapp
+    //const InterSpec *interspec = InterSpec::instance();
+    //const int ww = interspec->renderedWidth(), wh = interspec->renderedHeight();
+    //if( ww > 100 && wh > 100 )
+    //  setMaximumSize( WLength(std::max(ww/2,260), WLength::Pixel), WLength((9*wh)/10, WLength::Pixel) );
+    
+    Wt::WDialog::render( flags );
+    
+    // Override some WDialog settings
+    
+    // Set the dialog max-width
+    string maxw = " $('#" + id() + "').css('maxWidth', ($(window).width() * 0.5 | 0) + 'px' );";
+    doJavaScript( maxw );
+    
+    // Should probably set max-height as well
+    
+    // By default Wt sets the dialog-layout to 999999px if you dont set it in the C++ at object
+    //  construction, so we will over-ride this in a kinda hacky way so contents wont overflow the
+    //  dialog
+    doJavaScript( "$('#" + id() + " .dialog-layout').css('maxWidth','100%');" );
+    
+    // We could update the max-width when window size changes, but we'll ignore this for now
+    //doJavaScript( "$(window).bind('resize', function(){" + maxw + "} );" );
+  }else
+  {
+    Wt::WDialog::render( flags );
+  }//if( flags & RenderFull )
+}//render( flags )
+
+
+void SimpleDialog::init( const Wt::WString &title, const Wt::WString &content )
+{
+  wApp->useStyleSheet( "InterSpec_resources/SimpleDialog.css" );
+  
+  addStyleClass( "simple-dialog" );
+  
+  setModal( true );
+  
+  // Dont use the titlebar so the dialog wont be able to be moved around
+  setTitleBarEnabled( false );
+  if( !title.empty() )
+  {
+    WText *txt = new WText( title, contents() );
+    txt->setInline( false );
+    txt->addStyleClass( "title" );
+  }
+  
+  if( !content.empty() )
+  {
+    WText *txt = new WText( content, contents() );
+    txt->setInline( false );
+    txt->addStyleClass( "content" );
+  }
+  
+  setMinimumSize( WLength(260,WLength::Pixel), WLength::Auto );
+  // We will set maximum size in a hacky way in SimpleDialog::render(...)
+  
+  show();
+  finished().connect( boost::bind( &SimpleDialog::deleteSimpleDialog, this ) );
+}//init(...)
+
+
+SimpleDialog::~SimpleDialog()
+{
+  cerr << "Deleting simpledialog" << endl;
+}
+
+
+void SimpleDialog::deleteSimpleDialog( SimpleDialog *window )
+{
+  if( !window )
+    return;
+  
+  if( window->isModal() )
+    window->setModal(false);
+  
+  // We'll actually delete the windows later on in the event loop incase the order of connections
+  //  to its signals is out of intended order.
+  WServer::instance()->post( wApp->sessionId(), std::bind( [window](){
+    auto app = WApplication::instance();
+    if( app ){
+      delete window;
+      app->triggerUpdate();
+    }
+  } ) );
+}//deleteSimpleDialog
+
+
+Wt::WPushButton *SimpleDialog::addButton( const Wt::WString &txt )
+{
+  Wt::WPushButton *b = new WPushButton( txt, footer() );
+  b->setStyleClass( "simple-dialog-btn" );
+  
+  // TODO: closing the dialog seems a little laggy; check if WDialog::hide is faster, or if we
+  //       should stick to using the JS
+  //b->clicked().connect( this, &WDialog::hide );
+  b->clicked().connect( "function(){$('#" + id() + "').hide(); $('.Wt-dialogcover').hide();}" );
+  
+  b->clicked().connect( boost::bind( &WDialog::done, this, Wt::WDialog::DialogCode::Accepted ) );
+  return b;
+}
+
