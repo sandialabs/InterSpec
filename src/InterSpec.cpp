80621e79de83e1a2a989a11aa15f59f219b7aafb
William Johnson <wcjohns@sandia.gov>
2023-11-13 19:39:59 -0800

    Add option to fix peak mean to showing reference photopeak line on right-click menu.

--- src/InterSpec.cpp
+++ src/InterSpec.cpp
@@ -1042,6 +1042,12 @@ InterSpec::InterSpec( WContainerWidget *parent )
         m_rightClickMenutItems[i] = m_rightClickMenu->addMenuItem( "Use DRF FWHM" );
         m_rightClickMenutItems[i]->triggered().connect( this, &InterSpec::refitPeakWithDrfFwhm );
         break;
+        
+      case kSetMeanToRefPhotopeak:
+        m_rightClickMenutItems[i] = m_rightClickMenu->addMenuItem( "Set mean to photopeak" );
+        m_rightClickMenutItems[i]->triggered().connect( this, &InterSpec::setMeanToRefPhotopeak );
+        break;
+        
       case kRefitROI:
         m_rightClickMenutItems[i] = m_rightClickMenu->addMenuItem( "Refit ROI" );
         m_rightClickMenutItems[i]->triggered().connect( this, &InterSpec::refitPeakFromRightClick );
@@ -1814,6 +1820,12 @@ void InterSpec::refitPeakWithDrfFwhm()
 }//void InterSpec::refitPeakWithDrfFwhm()
 
 
+void InterSpec::setMeanToRefPhotopeak()
+{
+  PeakSearchGuiUtils::refit_peak_with_photopeak_mean( this, m_rightClickEnergy );
+}//void setMeanToRefPhotopeak()
+
+
 void InterSpec::addPeakFromRightClick()
 {
   UndoRedoManager::PeakModelChange peak_undo_creator;
@@ -2532,6 +2544,21 @@ void InterSpec::handleRightClick( double energy, double counts,
       case kRefitPeakWithDrfFwhm:
         m_rightClickMenutItems[i]->setHidden( !peak->gausPeak() );
       break;
+        
+      case kSetMeanToRefPhotopeak:
+      {
+        const float energy = PeakSearchGuiUtils::reference_line_energy_near_peak( this, *peak );
+        const bool hide = (energy < 10.0f);
+        m_rightClickMenutItems[i]->setHidden( hide );
+        if( !hide )
+        {
+          char buffer[64] = { '\0' };
+          snprintf( buffer, sizeof(buffer), "Mean to %.1f keV", energy );
+          m_rightClickMenutItems[i]->setText( buffer );
+        }//if( !hide )
+        
+        break;
+      }//case kSetMeanToRefPhotopeak:
         
       case kShareContinuumWithLeftPeak:
       {
