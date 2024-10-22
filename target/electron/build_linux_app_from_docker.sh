a4b427561fb2cf144fb344afeb5a938c4c71027f
William Johnson <wcjohns@sandia.gov>
2024-10-22 12:58:54 -0700

    CI iterate

--- target/electron/build_linux_app_from_docker.sh
+++ target/electron/build_linux_app_from_docker.sh
@@ -99,7 +99,9 @@ echo "Have copied app dir from ${CmakeBuildDir} to ${WorkingDir}/."
 
 echo "About to package"
 
-npm exec electron-packager app InterSpec --overwrite=true --platform=linux --arch=x64 --protocol=interspec --protocol-name="InterSpec" --icon="${InterSpecCodePath}/target/electron/linux/InterSpec_desktop_icon_256x256.png" --prune=true --ignore=LICENSE.md --ignore=README.md
+# The default version of npm in manylinux_2_28_x86_64 is 6.14.11, which doesnt support the `exec` command, so we'll locally install a newer version of npm
+npm install npm@10.9.0
+node_modules/.bin/npm exec electron-packager app InterSpec --overwrite=true --platform=linux --arch=x64 --protocol=interspec --protocol-name="InterSpec" --icon="${InterSpecCodePath}/target/electron/linux/InterSpec_desktop_icon_256x256.png" --prune=true --ignore=LICENSE.md --ignore=README.md
 
 cp "${InterSpecCodePath}/NOTICE.html" "${WorkingDir}/InterSpec-linux-x64/"
 echo "This is a build of InterSpec for Linux, using InterSpec code git hash ${GIT_HASH}." > "${WorkingDir}/InterSpec-linux-x64/build_information.txt"
