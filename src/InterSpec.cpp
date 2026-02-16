9fca877134b07c8c93b0b419465df68db6c2eb87
William Johnson <wcjohns@sandia.gov>
2026-02-16 11:07:46 -0800

    Cleaup CSS a little. Add app-menu-bar color related CSS variables, and related fields to set them in ColorThemeWidget. Remove a number of vestigual CSS style classes that look to have been removed from the C++ or JS, but not from the CSS. Fix text input fileds background staying black when going from dark to light color theme.

--- src/InterSpec.cpp
+++ src/InterSpec.cpp
@@ -4560,6 +4560,9 @@ void InterSpec::applyColorTheme( shared_ptr<const ColorTheme> theme )
     setNonChartCssVar( "button-background", theme->appButtonBackground );
     setNonChartCssVar( "button-border-color", theme->appButtonBorderColor );
     setNonChartCssVar( "button-text-color", theme->appButtonTextColor );
+    setNonChartCssVar( "menubar-background", theme->appMenuBarBackground );
+    setNonChartCssVar( "menubar-active-color", theme->appMenuBarActiveColor );
+    setNonChartCssVar( "menubar-hover-color", theme->appMenuBarHoverColor );
   }
 
   setReferenceLineColors( theme );
